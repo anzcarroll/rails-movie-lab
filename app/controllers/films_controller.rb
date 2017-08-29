ba6b2939a00af587278f78629384935e11700a64
anzcarroll <anzcarroll@gmail.com>
2017-08-29 16:00:42 -0400

    added a destroy

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -13,6 +13,22 @@ class FilmsController < ApplicationController
     redirect_to  film_path(@film) 
   end 
 
+  def edit
+    @film = Film.find params[:id]
+  end 
+
+  def update
+    @film = Film.find params[:id]
+    @film.update(film_params)
+    redirect_to film_path(@film)
+  end
+
+  def destroy 
+    @film = Film.find params[:id] 
+    @film.destroy
+    redirect_to films_path
+  end
+
     private
 
   def film_params
