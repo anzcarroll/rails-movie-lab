391ee922722369802b0dffd4f14cc1e2422c5241
anzcarroll <anzcarroll@gmail.com>
2017-08-29 15:36:53 -0400

    added a new form

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -5,4 +5,18 @@ class FilmsController < ApplicationController
   def show
     @film = Film.find params[:id]
   end
+  def new
+    @film = Film.new
+  end 
+  def create
+    @film = Film.create(film_params)
+    redirect_to  film_path(@film) 
+  end 
+
+    private
+
+  def film_params
+    params.require(:film).permit(:title, :genre, :year, :synopsis, :image)
+
+  end
 end
